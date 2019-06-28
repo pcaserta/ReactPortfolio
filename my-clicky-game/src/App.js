ce75c4b6b4222135e282d392655021b2a4aebe88
pcaserta <patrick.caserta@live.longwood.edu>
2019-06-28 17:03:52 -0400

    added title

--- my-clicky-game/src/App.js
+++ my-clicky-game/src/App.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Wrapper from "./components/wrapper";
-import Title from "./components/title"
+import Title from "./components/title";
 
 class App extends Component {
-
-render() {
-  return (
-   <Wrapper>
-  <Title> Clicky Game!</Title>
-</Wrapper>
-)};
+  render() {
+    return (
+      <Wrapper>
+        <Title> Clicky Game!</Title>
+      </Wrapper>
+    );
+  }
 }
 export default App;
