a7f408cb76cc969080ef81fcf50d70598ef75e7c
pcaserta <patrick.caserta@live.longwood.edu>
2019-07-01 19:46:12 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Title> Clicky Game! Click an image to begin <strong>Score: 0 | Top Score: 0</strong></Title>
+        <Title> Clicky Game! Click an image to beginnnn <strong>Score: 0 | Top Score: 0</strong></Title>
       </Wrapper>
-    );
-  }
+  )};
 }
 export default App;
