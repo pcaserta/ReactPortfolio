849027e4d7baac32cc2f6fc74060b762c5ae8e57
pcaserta <patrick.caserta@live.longwood.edu>
2019-09-12 18:34:35 -0400

    fixed bug

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
 
-class Landing extends Component {
+class LandingPage extends Component {
   render() {
     return (
       <div style={{ width: "100%", margin: "auto" }}>
@@ -66,4 +66,4 @@ class Landing extends Component {
   }
 }
 
-export default Landing;
+export default LandingPage;
