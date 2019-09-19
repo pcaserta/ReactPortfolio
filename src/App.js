c42ceecf6e805a0fdaec93bfb78bf064d61075af
pcaserta <patrick.caserta@live.longwood.edu>
2019-09-19 17:51:52 -0400

    update text for about me tab

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             >
               <Navigation>
                 <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About me</Link>
+                <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
               </Navigation>
