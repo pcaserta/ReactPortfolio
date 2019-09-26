de919b3eb5117c6ce2ec9eee9e67e1004620bf8e
pcaserta <patrick.caserta@live.longwood.edu>
2019-09-26 13:21:12 -0400

    name and title added to left div on resume page

--- src/components/resume.js
+++ src/components/resume.js
@@ -6,8 +6,20 @@ class Resume extends Component {
     return (
       <div>
         <Grid>
-          <Cell col={4}>Left Side</Cell>
-          <Cell col={8}>Right Side Side</Cell>
+          <Cell col={4}>
+            <div style={{ textAlign: "center" }}>
+              <img
+                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                alt="avatar"
+                style={{ height: "200px" }}
+              />
+            </div>
+            <h2 stle={{paddingTop: '2em'}}>Patrick Caserta</h2>
+            <h4 style={{color:'grey'}}>Programmer</h4>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            Right Side Side
+          </Cell>
         </Grid>
       </div>
     );
