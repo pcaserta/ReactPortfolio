2f5b444f1ea40526b3aff3706a34abc2f6fef33e
pcaserta <patrick.caserta@live.longwood.edu>
2019-09-26 13:39:28 -0400

    added education component for resume page

---
+++ src/components/education.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+
+class Education extends Component {
+  render() {
+    return (
+      <Grid>
+        <Cell col={4}>l</Cell>
+        <Cell col={8}>r</Cell>
+      </Grid>
+    );
+  }
+}
+
+export default Education;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
+import Education from "./education";
 
 class Resume extends Component {
   render() {
@@ -37,7 +38,8 @@ class Resume extends Component {
             <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
           </Cell>
           <Cell className="resume-right-col" col={8}>
-            Right Side Side
+            <h2>Education</h2>
+            <Education startYear={2012} />
           </Cell>
         </Grid>
       </div>
