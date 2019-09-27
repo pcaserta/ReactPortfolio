5102002707ef50ce554e1ef947027d0d91ac3bb5
pcaserta <patrick.caserta@live.longwood.edu>
2019-09-27 12:50:13 -0400

    added about me page content

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <h1>Abouttt Pageee</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="aboutme-grid">
+          <Cell col={12}>
+            <img
+              src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/malecostume-512.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <p
+              style={{
+                paddingTop: "2em",
+                paddingLeft: "30em",
+                paddingRight: "30em"
+              }}
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum
+            </p>
+          </Cell>
+        </Grid>
       </div>
     );
   }
