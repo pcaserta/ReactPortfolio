ba4cde2d82fa570d3518beb45f2caec2c0cd7c96
pcaserta <patrick.caserta@live.longwood.edu>
2019-09-27 12:20:12 -0400

    updated summary on resume page

--- src/components/resume.js
+++ src/components/resume.js
@@ -21,12 +21,13 @@ class Resume extends Component {
             <h4 style={{ color: "grey" }}>Programmer</h4>
             <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged.
+              I have been coding since I started my education at Longwood
+              University. I started with VisualBasic, SQL, and self-taught
+              Python. After entering the work force I realized that I wanted to
+              learn more and further my skills in the programming world. I chose
+              to take the web development path my signing up for the University
+              of Richmond Coding Boot Camp where I excelled and learned how to
+              build full-stack web applications.
             </p>
             <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
             <h5>Location</h5>
