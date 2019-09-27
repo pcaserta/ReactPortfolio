8b2045d19f9397926007889ff2b27a6b8b5514d2
pcaserta <patrick.caserta@live.longwood.edu>
2019-09-27 12:51:21 -0400

    Updates

--- precache-manifest.0b4ff45e94fcc8fdbe085096d1c0238e.js
+++ precache-manifest.0b4ff45e94fcc8fdbe085096d1c0238e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "371a4a49bc2a00d7c04677778eff04b9",
+    "revision": "92af8920bf8e68bc12baded59a1849f1",
     "url": "/React__Portfolio/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React__Portfolio/static/css/2.04aae6c4.chunk.css"
   },
   {
-    "revision": "7091f00d56d6af874c27",
-    "url": "/React__Portfolio/static/css/main.fd59786c.chunk.css"
+    "revision": "0548a747a8a9bea6c4cb",
+    "url": "/React__Portfolio/static/css/main.2e67e14f.chunk.css"
   },
   {
     "revision": "db2f241c08ad0d1d5163",
     "url": "/React__Portfolio/static/js/2.8753f293.chunk.js"
   },
   {
-    "revision": "7091f00d56d6af874c27",
-    "url": "/React__Portfolio/static/js/main.764d985e.chunk.js"
+    "revision": "0548a747a8a9bea6c4cb",
+    "url": "/React__Portfolio/static/js/main.59d875b6.chunk.js"
   },
   {
     "revision": "c8e509f1942f949151f5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React__Portfolio/precache-manifest.a461e49428fd83e59bdae8ef078db31e.js"
+  "/React__Portfolio/precache-manifest.0b4ff45e94fcc8fdbe085096d1c0238e.js"
 );
 
 self.addEventListener('message', (event) => {
